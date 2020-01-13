2a252a8e67952be9d8a94e1fd37e9947f8e59b83
mridula.bandi <mridula.bandi@infosys.com>
2020-01-13 14:41:39 +0530

    Initial Commit

---
+++ RiviFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiviFood
+//
+//  Created by mysmac_adm!n on 13/01/20.
+//  Copyright © 2020 Mridula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
